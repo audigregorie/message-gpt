4b266e83d3d24bcb4d101b7908b7211494f3873d
Audi Young <122812915+audigregorie@users.noreply.github.com>
2024-11-20 16:49:04 +0000

    format code block response from openai

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -3,7 +3,7 @@ import { useAuth } from '../../context/AuthContext';
 import { Role } from '../../enums/common.enum';
 import { ChatMessage } from '../../interface/common.interface';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 function extractCodeBlocks(message: string) {
   return message.split(/```/).map((block, index) => {
@@ -24,22 +24,37 @@ const ChatItem: React.FC<ChatMessage> = ({ role, content }) => {
   const messageBlocks = extractCodeBlocks(content);
   const auth = useAuth();
 
-  return (
-    <div className={`flex my-2 rounded-xl p-4 gap-4 items-start ${role === Role.Assistant ? 'bg-[rgb(17,27,39)]' : 'bg-[rgb(2,19,31)]'}`}>
+  return role === Role.Assistant ? (
+    <div className="flex my-2 rounded-xl p-4 gap-4 items-start bg-[rgb(17,27,39)]">
       <Avatar className="ml-0 !bg-black">
-        {role === Role.Assistant ? (
-          <img src="openai.png" alt="openai" className="w-8 invert" />
-        ) : (
-          <span className="text-white">
-            {auth?.user?.name[0]}
-            {auth?.user?.name.split(' ')[1]?.[0] || ''}
-          </span>
-        )}
+        <img src="openai.png" alt="openai" className="w-8 invert" />
       </Avatar>
       <div className="pt-2">
         {messageBlocks.map((block, index) =>
           block.isCode ? (
-            <SyntaxHighlighter key={index} style={atomDark} language={block.language}>
+            <SyntaxHighlighter key={index} style={coldarkDark} language={block.language}>
+              {block.content}
+            </SyntaxHighlighter>
+          ) : (
+            <p key={index} className="text-sm">
+              {block.content}
+            </p>
+          )
+        )}
+      </div>
+    </div>
+  ) : (
+    <div className="flex my-2 rounded-xl p-4 gap-4 items-start bg-[rgb(2,19,31)]">
+      <Avatar className="ml-0 !bg-black">
+        <span className="text-white">
+          {auth?.user?.name[0]}
+          {auth?.user?.name.split(' ')[1]?.[0] || ''}
+        </span>
+      </Avatar>
+      <div className="pt-2">
+        {messageBlocks.map((block, index) =>
+          block.isCode ? (
+            <SyntaxHighlighter key={index} style={coldarkDark} language={block.language}>
               {block.content}
             </SyntaxHighlighter>
           ) : (
