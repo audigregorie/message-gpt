b0cf0919ecefc2198b2908be8dac920c8b278f1e
Audi Young <122812915+audigregorie@users.noreply.github.com>
2024-11-25 05:32:17 +0000

    ready to deploy

--- client/src/utils/api/api-communicator.ts
+++ client/src/utils/api/api-communicator.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api/v1',
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api/v1',
   withCredentials: true
 });
 
